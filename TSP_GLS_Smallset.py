e51ce3fd4bf5e0f2e47b71ace8f13f0ed1a0e547
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2020-02-11 22:43:51 +0700

    Reformat to PEP

--- TSP_GLS_Smallset.py
+++ TSP_GLS_Smallset.py
@@ -2,18 +2,17 @@ import numpy as np
 import random
 import math
 
-def readData (path):
+def read_data(path, lines_of_comment=3):
     datafile = open(path, 'r')
 
     Name = datafile.readline().strip().split()[-1]
 
-    datafile.readline()
-    datafile.readline()
-    datafile.readline()
+    for _ in range(lines_of_comment):
+        datafile.readline()
 
-    Type = datafile.readline().strip().split()[-1]
+    _ = datafile.readline().strip().split()[-1]
     Dimension = datafile.readline().strip().split()[-1]
-    Weight_Type = datafile.readline().strip().split() [-1]
+    _ = datafile.readline().strip().split() [-1]
     datafile.readline()
 
     N = int(Dimension)
@@ -26,7 +25,7 @@ def readData (path):
 
 
 # Calculate the distance matrix
-def WeightMatrix (coord: np.ndarray):
+def weight_matrix (coord: np.ndarray):
     N = coord.shape[0]
     matrix = []
 
@@ -40,7 +39,7 @@ def WeightMatrix (coord: np.ndarray):
     return np.array(matrix, dtype = np.float64)
 
 
-def TourLength (weight: np.ndarray, solution: list):
+def tour_length(weight: np.ndarray, solution: list):
     length = weight[solution[-1], 0] + weight[0, solution[0]]
 
     for i in range(len(solution) - 1):
@@ -49,18 +48,18 @@ def TourLength (weight: np.ndarray, solution: list):
     return length
 
 
-def initSolution (n_cities: int):
+def init_solution(n_cities: int):
     s = np.arange(1, n_cities)
     random.shuffle(s)
     return list(s)
 
 
 # Features: whether the solution has the path from 0 to i, i = 1,2,...n_cities-1
-def hasFeature(solution, nextcity): # indicate whether the solution exhibit the feature
+def has_feature(solution, nextcity): # indicate whether the solution exhibit the feature
     return True if solution[0] == nextcity or solution[-1] == nextcity else False
 
 
-def LocalSearch (weight: np.ndarray, init_s: list, penalty: np.ndarray, eta: float, max_unimproved: int):
+def local_search(weight: np.ndarray, init_s: list, penalty: np.ndarray, eta: float, max_unimproved: int):
     n_cities = weight.shape[0]
     s = init_s
     n_unimproved = 0
@@ -114,7 +113,7 @@ def LocalSearch (weight: np.ndarray, init_s: list, penalty: np.ndarray, eta: flo
     return s
 
 
-def GuidedLocalSearch (weight: np.ndarray, eta: float, max_iter = 10000, max_unimproved = 100):
+def guided_local_search(weight: np.ndarray, eta: float, max_iter = 10000, max_unimproved = 100):
     n_cities = weight.shape[0]
 
     penalty = np.zeros(n_cities-1)
@@ -123,12 +122,12 @@ def GuidedLocalSearch (weight: np.ndarray, eta: float, max_iter = 10000, max_uni
     n_iter = 0
     n_unimproved = 0
 
-    current_s = initSolution(n_cities)
-    new_s = LocalSearch(weight, init_s = current_s, penalty = penalty, eta = eta, max_unimproved = max_unimproved)
+    current_s = init_solution(n_cities)
+    new_s = local_search(weight, init_s = current_s, penalty = penalty, eta = eta, max_unimproved = max_unimproved)
 
     while n_iter < max_iter or n_unimproved < max_unimproved:
         
-        if TourLength(weight, new_s) < TourLength(weight, current_s):
+        if tour_length(weight, new_s) < tour_length(weight, current_s):
             current_s = new_s
             n_unimproved = 0
         else:
@@ -136,12 +135,12 @@ def GuidedLocalSearch (weight: np.ndarray, eta: float, max_iter = 10000, max_uni
         
 
         for i in range(len(utility)):
-            if hasFeature(current_s, i+1):
+            if has_feature(current_s, i+1):
                 utility[i] = weight[0, i+1] / (1 + penalty[i])
         
         penalty = np.where(penalty == max(penalty), penalty+1, penalty)
         
-        new_s = LocalSearch(weight, init_s= current_s, penalty = penalty, eta = eta, max_unimproved = max_unimproved)
+        new_s = local_search(weight, init_s= current_s, penalty = penalty, eta = eta, max_unimproved = max_unimproved)
         
         n_iter += 1
 
@@ -167,13 +166,13 @@ paths = [path_1, path_2, path_3, path_4, path_5]
 csvData = [['Data set', 'Best', 'Worst', 'Average', 'Deviation']]
 
 for path in paths:
-    coord, name = readData(path)
-    w = WeightMatrix(coord)
+    coord, name = read_data(path)
+    w = weight_matrix(coord)
 
     history = np.empty(30)
     for i in range(30):
-        opt = GuidedLocalSearch(w, eta, max_iter, max_unimproved)
-        history[i] = TourLength(w, opt)
+        opt = guided_local_search(w, eta, max_iter, max_unimproved)
+        history[i] = tour_length(w, opt)
     
     best = min(history)
     worst = max(history)
